4b46770ee4f18a5bda032f8db48aa11c2eb57eb1
Bidur Adhikari <badhikari@godaddy.com>
2018-04-02 23:11:46 -0700

    Implement using setInterval

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Hello(){
+  let currentTime = new Date();
   return (
-    <div>Hello React App</div>
+    <div>
+      <div>Welcome to my clock</div>
+    <div>{currentTime.toLocaleTimeString()}</div>
+    </div>
   );
 }
-
 // ========================================
 
-ReactDOM.render(
-  <Hello />,
-  document.getElementById('root')
-);
+setInterval(function(){
+  ReactDOM.render(
+    <Hello />,
+    document.getElementById('root')
+  );
+}, 1000);
