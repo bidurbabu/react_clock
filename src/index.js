bec8e0fd77f8dfa63d9b4d06c67898072a3af5fc
Bidur Adhikari <badhikari@godaddy.com>
2018-04-02 23:25:58 -0700

    Single time display with state and class

--- src/index.js
+++ src/index.js
@@ -2,20 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Hello(){
-  let currentTime = new Date();
-  return (
-    <div>
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+  }
+  render() {
+    return (<div>
       <div>Welcome to my clock</div>
-    <div>{currentTime.toLocaleTimeString()}</div>
-    </div>
-  );
+      <div>{this.state.date.toLocaleTimeString()}</div>
+    </div>);
+  }
+
 }
 // ========================================
 
-setInterval(function(){
-  ReactDOM.render(
-    <Hello />,
-    document.getElementById('root')
-  );
-}, 1000);
+ReactDOM.render(<Clock/>, document.getElementById('root'));
