add9759ae4b726e11b76299dac7f70ee06fdc5b9
Bidur Adhikari <badhikari@godaddy.com>
2018-04-11 11:03:37 -0700

    Added new digital clock for Kathmandu Time zone

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function KathmanduTime(props) {
+  return (
+    <div>
+      <div>Current Date and time in Kathmandu Nepal is {props
+          .date
+          .toLocaleTimeString('en-US', {timeZone: 'Asia/Kathmandu'})}</div>
+    </div>
+  )
+}
+function LocalTime(props) {
+  return (
+    <div>
+      <div>Current Local Date and time is {props
+          .date
+          .toLocaleTimeString()}</div>
+    </div>
+  )
+}
 class Clock extends React.Component {
   constructor(props) {
     super(props);
@@ -10,27 +28,26 @@ class Clock extends React.Component {
     };
   }
   componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
+    this.timerID = setInterval(() => this.tick(), 1000);
   }
   componentWillUnmount() {
     clearInterval(this.timerID);
   }
   tick() {
-    this.setState({
-      date: new Date()
-    });
+    this.setState({date: new Date()});
   }
   render() {
-    return (<div>
-      <div>Welcome to my clock</div>
-      <div>{this.state.date.toLocaleTimeString()}</div>
-    </div>);
+    return (
+      <div>
+        <div>Welcome to my clock</div>
+        <LocalTime date={this.state.date}/>    
+        <KathmanduTime date={this.state.date}/>
+      </div>
+    );
   }
 
 }
 // ========================================
 
-ReactDOM.render(<Clock/>, document.getElementById('root'));
+ReactDOM.render(
+  <Clock/>, document.getElementById('root'));
